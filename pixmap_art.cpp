8f41d53766b37f2407a945c3f5633b5c99549747
ikhovryak <ikhovryak@haverford.edu>
2021-03-08 23:57:58 -0500

    Fully done with all the code

--- pixmap_art.cpp
+++ pixmap_art.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include "ppm_image.h"
 using namespace std;
+using namespace agl;
 
 int main(int argc, char** argv)
 {
+	ppm_image earth;
+	earth.load("../images/earth-ascii.ppm");
+
+	float threshold = 0.8;
+	float alpha = 0.5;
+	ppm_image mainColors = earth.extractMainColors();
+	ppm_image whites = earth.extractWhite(threshold);
+
+	ppm_image filtered_by_Iryna = mainColors.lightest(whites, alpha);
+
+	// Filter as a function
+	// ppm_image filtered_by_Iryna = earth.IrynasFilter(0.8f, 0.5f); 
+	filtered_by_Iryna.save("earth-filtered_by_Iryna.ppm");
 }
 
